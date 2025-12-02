b0ea51704fc899daa9c869ade7d37872a6c522ff
Marcos Corazza <mcs.corazza@gmail.com>
2025-12-02 17:08:01 -0500

    Adiciona projeto inicial SwiftUI na pasta swift_ui

---
+++ swift_ui/Aula2_Swift/Aula2_Swift/Aula2_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula2_SwiftApp.swift
+//  Aula2_Swift
+//
+//  Created by user288580 on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Aula2_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift_ui/Aula2_Swift/Aula2_Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aula2_Swift
+//
+//  Created by user288580 on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
