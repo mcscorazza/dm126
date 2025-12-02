6c89f152ad0551534857067392122260b4b7abfe
Marcos Corazza <mcs.corazza@gmail.com>
2025-12-02 18:48:30 -0500

    Criada tela de Login no SwiftUI|XCode

--- swift_ui/Aula2_Swift/Aula2_Swift/ContentView.swift
+++ swift_ui/Aula2_Swift/Aula2_Swift/ContentView.swift
@@ -2,18 +2,50 @@
 //  ContentView.swift
 //  Aula2_Swift
 //
-//  Created by user288580 on 12/2/25.
+//  Created by Marcos Corazza on 12/2/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username:String = ""
+    @State private var password:String = ""
+    @State private var alertMessage:String = ""
+    @State private var showAlert:Bool = false
+    
     var body: some View {
-        HStack{
-            Button("H") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
+        VStack(spacing:20){
+            TextField(
+                "username", text: $username
+            ).frame(maxWidth: 200)
+                .textFieldStyle(.roundedBorder)
+            
+            HStack{
+                SecureField(
+                    "password", text: $password
+                ).frame(maxWidth: 200)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button(action: handleLogin) {
+                    Image(systemName: "greaterthan.square.fill")
+                }
+            }.frame(maxWidth: 200)
+                                           
+        }.frame(maxHeight:200)
+        
+        .alert(alertMessage, isPresented: $showAlert) {
+            Button("Ok", role: .cancel){}
         }
+    }
+    
+    private func handleLogin() {
+        if username == "Admin" && password == "123" {
+            alertMessage = "Login feito com sucesso!"
+        } else {
+            alertMessage = "Credenciais invalidas!"
+        }
+        
+        showAlert = true
     }
 }
 
